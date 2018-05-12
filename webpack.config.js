aefe12f704e51f9598f47b5591c0f775e73e2113
cynthialu42 <clu2334@gmail.com>
2018-05-12 10:09:27 -0500

    got a button

---
+++ webpack.config.js
@@ -0,0 +1,29 @@
+module.exports = [{
+    entry: './app.scss',
+    output: {
+      // This is necessary for webpack to compile
+      // But we never use style-bundle.js
+      filename: 'style-bundle.js',
+    },
+    module: {
+      rules: [{
+        test: /\.scss$/,
+        use: [
+          {
+            loader: 'file-loader',
+            options: {
+              name: 'bundle.css',
+            },
+          },
+          { loader: 'extract-loader' },
+          { loader: 'css-loader' },
+          {
+            loader: 'sass-loader',
+            options: {
+                sourceMap: true,
+                includePaths: ['./node_modules']            }
+          },
+        ]
+      }]
+    },
+}];
